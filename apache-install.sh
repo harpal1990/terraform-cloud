a02bee526f5d451c03186bd9c0abc93c22d6005b
ktpl-harpal <harpalgohilon@gmail.com>
2023-07-28 10:00:12 +0530

    update files

--- apache-install.sh
+++ apache-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl enable httpd
-sudo service httpd start  
-echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" | sudo tee /var/www/html/index.html
+sudo apt update 
+sudo apt-get install nginx -y 
+sudo systemctl enable nginx 
+sudo service nginx start  
+echo "<h1>Welcome to techserver! AWS Infra created using Terraform in us-east-1 Region</h1>" | sudo tee /var/www/html/index.html
